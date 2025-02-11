bba73c0822a1c9ce2a33ae19551b7725b6b5345a
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-11 21:00:28 +0700

    Cập nhật code
    thêm dòng và sửa dòng đã có

--- HelloWorld/src/ntu/XinChao.java
+++ HelloWorld/src/ntu/XinChao.java
@@ -13,8 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// 
-		System.out.println("Xin chào. Tôi là SV Khóa 64 CNTT-NTU: Tôi lần đầu học Java và đang thử nghiệm quản lý mã nguồn với GIT");
-		
+		System.out.println("Xin chào. Tôi là SV Khóa 64 CNTT-NTU: Tôi lần đầu học lập trình Java và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.println("Nguyễn Nhật Trung - 64.TTMMT");
 	}
 
 }
