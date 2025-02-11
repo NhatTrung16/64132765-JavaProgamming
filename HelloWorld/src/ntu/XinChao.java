7ef18f8ec34ed00db88967d02ff7eb973485efcd
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-11 20:58:15 +0700

    Ví dụ Hello
    Đã làm được ví dụ Hello ban đầu

---
+++ HelloWorld/src/ntu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ntu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Xin chào. Tôi là SV Khóa 64 CNTT-NTU: Tôi lần đầu học Java và đang thử nghiệm quản lý mã nguồn với GIT");
+		
+	}
+
+}
