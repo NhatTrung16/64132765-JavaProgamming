c8b816e81f3a0b6802cf9d8d6068323de323a83b
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-17 10:49:02 +0700

    Nhập và xuất thông tin sinh viên
    Nhập và xuất thông tin sinh viên gồm họ tên và điểm trung bình

--- Lab1_Bai1_DTB/src/lab1/DTB.java
+++ Lab1_Bai1_DTB/src/lab1/DTB.java
@@ -1,10 +1,21 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class DTB {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nhập họ và tên sinh viên: ");
+		String hoten = sc.nextLine();
+		
+		System.out.print("Nhập điểm trung bình: ");
+		double dtb = sc.nextDouble();
+		
+		System.out.println("\nThông tin sinh viên");
+		System.out.println("Họ tên sinh viên: "+hoten);
+		System.out.println("Điểm trung bình: "+dtb);
 	}
-
 }
