fc7ff15ee35354faca225be4e4bbf7021d7d6f8e
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-17 11:00:43 +0700

    Tính thể tích khối lập phương
    Nhập cạnh của khối lập phương, tính và xuất thể tích của khối lập phương

--- Lab1_Bai3_TheTich/src/lab1/TinhTheTich.java
+++ Lab1_Bai3_TheTich/src/lab1/TinhTheTich.java
@@ -1,10 +1,18 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class TinhTheTich {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nhập cạnh của khối lập phương: ");
+		double canh = sc.nextDouble();
+		
+		double thetich = Math.pow(canh, 3);
+		
+		System.out.println("\nThể tích của khối lập phương: "+thetich);
 	}
 
 }
