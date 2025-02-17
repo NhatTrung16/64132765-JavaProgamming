e345079c7b42706650919fd79e97918af6f9879a
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-17 10:36:59 +0700

    Tính và xuất chu vi diện tích HCN
    Tính và xuất chu vi diện tích của HCN dữ liệu nhập vào từ bàn phím

--- Lab1_Bai2_ChuviDT/src/lab1/TinhCVDT.java
+++ Lab1_Bai2_ChuviDT/src/lab1/TinhCVDT.java
@@ -1,10 +1,29 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class TinhCVDT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("");
+		
+		
+		//Ready for input
+		Scanner banPhim = new Scanner(System.in);
+		
+		//Input
+		System.out.print("Nhập chiều dài của hình chữ nhật: ");
+		double a= banPhim.nextDouble();
+		
+		System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+		double b= banPhim.nextDouble();
+		
+		double chuvi = 2 * (a+b);
+		double dtich = a*b;
+		
+		//Output
+		System.out.println("Chu vi hình chữ nhật: "+ chuvi);
+		System.out.println("Diện tích hình chữ nhật: "+ dtich);
 	}
 
 }
