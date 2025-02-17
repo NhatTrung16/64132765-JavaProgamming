8ade6c2a6536af801d459dc58c16a97285ed2b6e
Nhật Trung Nguyễn <kenkenhallo16@gmail.com>
2025-02-17 11:21:23 +0700

    Nhập và tính xuất căn bậc hai delta
    Nhập giá trị phương trình bậc 2, tính delta và xuất căn bậc 2 delta

--- Lab1_Bai4_Delta/src/lab1/TinhDelta.java
+++ Lab1_Bai4_Delta/src/lab1/TinhDelta.java
@@ -1,9 +1,32 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class TinhDelta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Phương trình bậc 2");
+		System.out.print("\nNhập a: ");
+		double a = sc.nextDouble();
+		
+		System.out.print("\nNhập b: ");
+		double b = sc.nextDouble();
+		
+		System.out.print("\nNhập c: ");
+		double c = sc.nextDouble();
+		
+		double delta = (b*b) - 4*(a*c);
+		double sqrtdelta = Math.sqrt(delta);
+		
+		if(delta >=0) {
+			System.out.println("Căn bậc hai delta: "+sqrtdelta );
+		} else {
+			System.out.println("Delta âm, không có giá trị căn bậc 2 delta ");
+		}
+		
 
 	}
 
